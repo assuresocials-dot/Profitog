3db65dae339a1a5f37a0bf770dc6f917a24b6b5c
assuresocials-dot <assuresocials@gmail.com>
2025-08-07 12:03:38 +0200

    Add config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = "8259646914:AAHFPyneaR1_wifA6UjF4JbmyEFnDlANAes"
+NOWPAYMENTS_API_KEY = "dffKehGiXihgtVI9b7/Iw5IQvJca3DWL"
+
+# Your wallet addresses (for info display)
+WALLETS = {
+    "BTC": "bc1qsw57f9hkyfa73qlu2ygqxfdfz6j34jl7nl3qkj",
+    "ETH": "0x262b84049e4093Ed48465d6f160aE91Fd5623B96",
+    "SOL": "CTfqeVgQj2To3hSsa3ZUSQVmr467WXnueanAPi6UBpYp"
+}
